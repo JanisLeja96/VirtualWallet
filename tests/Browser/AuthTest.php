695508102ea0a037b2ee1eb1baf8f6bac03d859a
JanisLeja1996 <janis.leja1996@gmail.com>
2020-10-29 13:21:00 +0200

    Added browser tests

---
+++ tests/Browser/AuthTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class AuthTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    public function testUserCanRegister()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/register')
+                ->type('username', 'TestUser')
+                ->type('fullname', 'Test User')
+                ->type('password', '123')
+                ->type('password_confirmation', '123')
+                ->click('@register')
+                ->assertPathIs('/wallets');
+        });
+    }
+
+    public function testAllFieldsRequiredForRegistration()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/register')
+                ->type('username', 'TestUser')
+                ->type('password', '123')
+                ->type('password_confirmation', '123')
+                ->click('@register')
+                ->assertPathIs('/register');
+        });
+    }
+
+    public function testUsernameMustBeUnique()
+    {
+        User::factory()->create(['username' => 'TestUser']);
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/register')
+                ->type('username', 'TestUser')
+                ->type('fullname', 'Test User')
+                ->type('password', '123')
+                ->type('password_confirmation', '123')
+                ->click('@register')
+                ->assertPathIs('/register');
+        });
+    }
+
+}
