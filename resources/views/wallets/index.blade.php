2b9a49f3f0670ddc20f15cbbf9c26793187b3fe9
JanisLeja1996 <janis.leja1996@gmail.com>
2020-10-29 13:23:48 +0200

    Fixed button position

--- resources/views/wallets/index.blade.php
+++ resources/views/wallets/index.blade.php
@@ -11,9 +11,9 @@
                 </a>
 
             @endforeach
-            <div class="flex justify-center mr-2 mb-2 ml-auto">
+            <div class="flex justify-center">
                 <form method="get" action="{{ route('createWallet') }}">
-                    <button class="mt-12 w-32 h-12 bg-blue-300 items-end rounded-full text-sm border border-gray-800" type="submit">Create new wallet</button>
+                    <button class="mt-12 w-32 h-12 bg-blue-300 rounded-full text-sm border border-gray-800" type="submit">Create new wallet</button>
                 </form>
             </div>
         </div>
